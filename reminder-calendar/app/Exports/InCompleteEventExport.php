1bc374942c17e6fc0b7dd51dd5b12c2c28fba2d2
unknown <bangphan3031@gmail.com>
2023-06-15 01:28:43 +0700

    update

--- reminder-calendar/app/Exports/InCompleteEventExport.php
+++ reminder-calendar/app/Exports/InCompleteEventExport.php
@@ -25,9 +25,8 @@ class InCompleteEventExport implements FromCollection, WithHeadings, WithMapping
         return [
             'Calendar',
             'Title',
-            'Is All Day',
-            'Start Time',
-            'End Time',
+            'Date',
+            'Time',
             'Organizer',
             'Created date',
         ];
@@ -35,14 +34,36 @@ class InCompleteEventExport implements FromCollection, WithHeadings, WithMapping
 
     public function map($event): array
     {
+        $start_time = $event->start_time; 
+        $end_time = $event->end_time; 
+        $is_all_day = $event->is_all_day; 
+
+        $start_date = date('d-m-Y', strtotime($start_time));
+        $end_date = date('d-m-Y', strtotime($end_time));
+
+        if ($start_date == $end_date) {
+            $date = $start_date;
+        } else {
+            $date = $start_date . ' - ' . $end_date;
+        }
+
+        if ($is_all_day == 1) {
+            $time = 'all day';
+        } elseif ($start_time != $end_time) {
+            $start_time_formatted = date('h:i a', strtotime($start_time));
+            $end_time_formatted = date('h:i a', strtotime($end_time));
+            $time = $start_time_formatted . ' - ' . $end_time_formatted;
+        } else {
+            $time = date('h:i a', strtotime($start_time));
+        }
+
         return [
             $event->creator_calendar ? $event->creator_calendar : $event->name,
             $event->title,
-            $event->is_all_day,
-            $event->start_time,
-            $event->end_time,
+            $date,
+            $time,
             $event->creator ? $event->creator : 'Me',
-            $event->created_at,
+            $event->updated_at,
         ];
     }
 }
